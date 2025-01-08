beb0cc5805a7166eba75b1227f40fdc1ac280e14
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-01-08 18:37:30 +0800

    angid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
+import { use } from "react";
 uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [filterState, setFilterState] = useState("All");
+  const [logdisplay, setDisplay] = useState("none");
+  const [selectedLog, setSelectedLog] = useState("All");
+  const [select, setSelected] = useState("off");
+
+
 
   function handleChange(e) {
     setInputValue(e.target.value);
@@ -17,7 +23,7 @@ function App() {
       return;
     }
     e.preventDefault();
-    const currentDate = new Date().toLocaleString(); // Format the date
+    const currentDate = new Date().toLocaleString();
     setTodos([
       ...todos,
       {
@@ -61,191 +67,281 @@ function App() {
   };
 
   const handleDeleteAll = () => {
+    const currentDate = new Date().toLocaleDateString();
     const tasks = todos.map((todo) => {
       if (todo.status == "Completed") {
-        return { ...todo, status: "Deleted" };
+        return { ...todo, status: "Deleted", deletedDate: currentDate };
       } else {
         return todo;
       }
     });
     setTodos(tasks);
   };
+
+  const logDisplayBlock = (display) => {
+    setDisplay(display);
+    console.log(display);
+  };
   const filteredTodos = todos.filter((todo) => todo.status !== "Deleted");
-  console.log(filteredTodos);
   const completedTodos = todos.filter((todo) => todo.status == "Completed");
   const activeTodos = todos.filter((todo) => todo.status == "Active");
+
+  
+  const selectON = (select) => {
+    if (selectedLog== "Deleted") {
+    setSelected(select);
+    }
+    const tasks = todos.map((todo) => {
+      if (todo.select == "selected") {
+          return {...todo, select:null}
+        
+      } else {
+        return todo;
+      }
+    });
+    setTodos(tasks);
+    console.log(select);
+  };
+  const selectDeletedTask = (id) => {
+    if (select == "on" && selectedLog== "Deleted") {
+      const tasks = todos.map((todo) => {
+        if (todo.id == id && todo.select == "selected") {
+          return { ...todo, select: null };
+        }else if(todo.id == id) {
+          return {...todo, select:"selected" }
+        } 
+        else {
+          return todo;
+        }
+      });
+      setTodos(tasks);
+      console.log(tasks);
+    }
+  };
+  const recoverDeletedTask = () => {
+    const tasks = todos.map((todo) => {
+      if (todo.select == "selected") {
+          return {...todo, status:"Active", deletedDate : null, select:null}
+        
+      } else {
+        return todo;
+      }
+    });
+    setTodos(tasks);
+  };
 
   return (
     <>
       <div className="app">
-        <div className="board">
-          <div className="container">
-            <div className="tittle">
-              <p>To-Do list</p>
+        {
+          <div className="board">
+            <div className="container">
+              <div className="tittle">
+                <p>To-Do list</p>
+              </div>
+              <div className="inputContainer">
+                <input
+                  placeholder="Add tasks.."
+                  className="input"
+                  value={inputValue}
+                  onChange={handleChange}
+                />
+                <button
+                  className="button"
+                  id="selectedButton"
+                  onClick={handleSubmit}
+                >
+                  <p>Add</p>
+                </button>
+              </div>
+              <div className="filterContainer">
+                <button
+                  className="button"
+                  onClick={() => handleFilterStateChange("All")}
+                  style={{
+                    background:
+                      filterState == "All" ? "#3c82f6" : "rgba(0, 0, 0, .1",
+                    color: filterState == "All" ? "white" : "#363636",
+                  }}
+                >
+                  All
+                </button>
+                <button
+                  className="button"
+                  onClick={() => handleFilterStateChange("Active")}
+                  style={{
+                    background:
+                      filterState == "Active" ? "#3c82f6" : "rgba(0, 0, 0, .1",
+                    color: filterState == "Active" ? "white" : "#363636",
+                  }}
+                >
+                  Active
+                </button>
+                <button
+                  className="button"
+                  onClick={() => handleFilterStateChange("Completed")}
+                  style={{
+                    background:
+                      filterState == "Completed"
+                        ? "#3c82f6"
+                        : "rgba(0, 0, 0, .1",
+                    color: filterState == "Completed" ? "white" : "#363636",
+                  }}
+                >
+                  Completed
+                </button>
+                {todos.length > 0 ? (
+                  <button
+                    className="button"
+                    onClick={() => logDisplayBlock("block")}
+                    id="log"
+                  >
+                    log
+                  </button>
+                ) : null}
+              </div>
+              <div className="taskCont" id="task">
+                {todos
+                  .filter((todo) => {
+                    if (filterState == "All") {
+                      return todo.status !== "Deleted";
+                    } else if (filterState == "Completed") {
+                      return todo.status == "Completed";
+                    } else if (filterState == "Active") {
+                      return todo.status == "Active";
+                    } else {
+                      return true;
+                    }
+                  })
+                  .map((todo) => (
+                    <div className="task" key={todo.id}>
+                      <input
+                        type="checkbox"
+                        className="checkBox"
+                        checked={todo.status === "Completed"}
+                        onChange={() => handleTaskChekBox(todo.id)}
+                      />
+                      <div>
+                        <p>{todo.description}</p>
+                      </div>
+                      <button
+                        className="deleteButton"
+                        onClick={() => handleDelete(todo.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  ))}
+                {filteredTodos.length == 0 && filterState == "All" ? (
+                  <p className="blutText">No tasks yet. Add one above!</p>
+                ) : null}
+                {completedTodos.length == 0 && filterState == "Completed" ? (
+                  <p className="blutText">No active tasks found.</p>
+                ) : null}
+                {activeTodos.length == 0 && filterState == "Active" ? (
+                  <p className="blutText">No completed tasks found.</p>
+                ) : null}
+              </div>
+              {filteredTodos.length !== 0 ? (
+                <div className="summaryCount">
+                  <div className="count">
+                    {completedTodos.length} of {filteredTodos.length} tasks
+                    completed
+                  </div>
+                  {completedTodos.length !== 0 ? (
+                    <button
+                      className="clearCompleted"
+                      onClick={() => handleDeleteAll()}
+                    >
+                      Clear Completed
+                    </button>
+                  ) : null}
+                </div>
+              ) : null}
             </div>
-            <div className="inputContainer">
-              <input
-                placeholder="Add tasks.."
-                className="input"
-                value={inputValue}
-                onChange={handleChange}
-              />
-              <button
-                className="button"
-                id="selectedButton"
-                onClick={handleSubmit}
-              >
-                <p>Add</p>
-              </button>
+            <div className="footContainer">
+              <p>Powered by</p>
+              <a href="">
+                <p className="pinecone">Pinecone academy</p>
+              </a>
             </div>
-            <div className="filterContainer">
-              <button
-                className="button"
-                onClick={() => handleFilterStateChange("All")}
-                style={{
-                  background:
-                    filterState == "All" ? "#3c82f6" : "rgba(0, 0, 0, .1",
-                  color: filterState == "All" ? "white" : "#363636",
-                }}
+          </div>
+        }
+        {logdisplay == "block" ? (
+          <div className="allTasksContainer">
+            <button
+              className="removeLog"
+              onClick={() => logDisplayBlock("")}
+            ></button>
+        
+
+            <div>
+              <h3>Logs</h3>
+              <select
+                name="logSelect"
+                id="secectLog"
+                className="selection"
+                value={selectedLog}
+                onChange={(e) => setSelectedLog(e.target.value)}
               >
-                All
-              </button>
-              <button
-                className="button"
-                onClick={() => handleFilterStateChange("Active")}
-                style={{
-                  background:
-                    filterState == "Active" ? "#3c82f6" : "rgba(0, 0, 0, .1",
-                  color: filterState == "Active" ? "white" : "#363636",
-                }}
-              >
-                Active
-              </button>
-              <button
-                className="button"
-                onClick={() => handleFilterStateChange("Completed")}
-                style={{
-                  background:
-                    filterState == "Completed" ? "#3c82f6" : "rgba(0, 0, 0, .1",
-                  color: filterState == "Completed" ? "white" : "#363636",
-                }}
-              >
-                Completed
-              </button>
-            </div>
-            <div className="taskCont" id="task">
+                <option value="All">All</option>
+                <option value="Active">Active</option>
+                <option value="Completed">Completed</option>
+                <option value="Deleted">Deleted</option>
+              </select>
               {todos
                 .filter((todo) => {
-                  if (filterState == "All") {
-                    return todo.status !== "Deleted";
-                  } else if (filterState == "Completed") {
-                    return todo.status == "Completed";
-                  } else if (filterState == "Active") {
+                  if (selectedLog == "Active") {
                     return todo.status == "Active";
+                  } else if (selectedLog == "Completed") {
+                    return todo.status == "Completed";
+                  } else if (selectedLog == "Deleted") {
+                    return todo.status == "Deleted";
                   } else {
                     return true;
                   }
                 })
                 .map((todo) => (
-                  <div className="task" key={todo.id}>
-                    <input
-                      type="checkbox"
-                      className="checkBox"
-                      checked={todo.status === "Completed"}
-                      onChange={() => handleTaskChekBox(todo.id)}
-                    />
-                    <div>
-                      <p>{todo.description}</p>
-                      {todo.addedDate && (
-                        <p className="dateLog">Added: {todo.addedDate}</p>
-                      )}
-                      {todo.completedDate && (
-                        <p className="dateLog">
-                          Completed: {todo.completedDate}
-                        </p>
-                      )}
-                      {todo.deletedDate && (
-                        <p className="dateLog">Deleted: {todo.deletedDate}</p>
-                      )}
-                    </div>
-                    <button
-                      className="deleteButton"
-                      onClick={() => handleDelete(todo.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                ))}
-              {filteredTodos.length == 0 && filterState == "All" ? (
-                <p className="blutText">No tasks yet. Add one above!</p>
-              ) : null}
-              {completedTodos.length == 0 && filterState == "Completed" ? (
-                <p className="blutText">No active tasks found.</p>
-              ) : null}
-              {activeTodos.length == 0 && filterState == "Active" ? (
-                <p className="blutText">No completed tasks found.</p>
-              ) : null}
-            </div>
-            {filteredTodos.length !== 0 ? (
-              <div className="summaryCount">
-                <div className="count">
-                  {completedTodos.length} of {filteredTodos.length} tasks
-                  completed
-                </div>
-                {completedTodos.length !== 0 ? (
-                  <button
-                    className="clearCompleted"
-                    onClick={() => handleDeleteAll()}
+                  <div
+                    key={todo.id}
+                    className="taskLog"
+                    onClick={() => selectDeletedTask(todo.id)}
+                    style={{
+                      border:
+                        todo.select == "selected" && select == "on"
+                          ? "solid"
+                          : "white",
+                    }}
                   >
-                    Clear Completed
-                  </button>
-                ) : null}
-              </div>
-            ) : null}
-            <div className="allTasksContainer">
-              <h2>All Tasks</h2>
-              <div>
-                <h3>Active</h3>
-                {todos
-                  .filter((todo) => todo.status === "Active")
-                  .map((todo) => (
-                    <div key={todo.id} className="task">
-                      <p>{todo.description}</p>
+                    <div>
+                      <p>description : {todo.description}</p>
+                    </div>
+                    <div>
                       <p className="dateLog">Added: {todo.addedDate}</p>
-                    </div>
-                  ))}
-              </div>
-              <div>
-                <h3>Completed</h3>
-                {todos
-                  .filter((todo) => todo.status === "Completed")
-                  .map((todo) => (
-                    <div key={todo.id} className="task">
-                      <p>{todo.description}</p>
                       <p className="dateLog">Completed: {todo.completedDate}</p>
-                    </div>
-                  ))}
-              </div>
-              <div>
-                <h3>Deleted</h3>
-                {todos
-                  .filter((todo) => todo.status === "Deleted")
-                  .map((todo) => (
-                    <div key={todo.id} className="task">
-                      <p>{todo.description}</p>
                       <p className="dateLog">Deleted: {todo.deletedDate}</p>
                     </div>
-                  ))}
-              </div>
+                  </div>
+                ))}
             </div>
+            <div className="buttonCont">{selectedLog == "Deleted" ? (
+              <button className="logButton" onClick={() => selectON("on")}>Select</button>
+            ) : null}
+            {select == "on" && selectedLog=="Deleted" ? (
+              <button className="logButton" onClick={() => selectON("off")}>
+                Cancel
+              </button>
+            ) : null}
+            {select == "on" && selectedLog == "Deleted" ? (
+              <button
+                className="logButton"
+                onClick={() => recoverDeletedTask()}
+              >
+                Recover
+              </button>
+            ) : null}</div>
           </div>
-          <div className="footContainer">
-            <p>Powered by</p>
-            <a href="">
-              <p className="pinecone">Pinecone academy</p>
-            </a>
-          </div>
-        </div>
+        ) : null}
       </div>
     </>
   );
